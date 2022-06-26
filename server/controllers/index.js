a0a26dafcfedbda9c52085ea47140aa972c3ba8e
Louvre Chow <louvrecly@gmail.com>
2022-06-25 23:18:19 -0400

    Import scanSurroundings service into action method in controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,4 +1,4 @@
-import { scanArena, checkEnemyInRange } from "../services";
+import { scanArena, checkEnemyInRange, scanSurroundings } from "../services";
 
 function get(req, res) {
   return res.send('Let the battle begin!');
@@ -12,10 +12,11 @@ function action(req, res) {
 
   const arena = scanArena(dims, state);
   const ownState = state[href];
+  const surroundings = scanSurroundings(ownState, arena, dims);
 
   if (ownState.wasHit) {
     return res.send(['F', 'L', 'R'][Math.floor(Math.random() * 3)]);
-  } else if (checkEnemyInRange(ownState, arena, dims)) {
+  } else if (checkEnemyInRange(surroundings)) {
     return res.send('T');
   } else {
     return res.send(['L', 'R'][Math.floor(Math.random()) * 2]);
