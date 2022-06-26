beff2aaa4e8696bdaac91279d9c29516548f2a13
Louvre Chow <louvrecly@gmail.com>
2022-06-26 12:00:37 -0400

    Create requestValidator middleware to defend against greetings

---
+++ server/middleware/requestValidator.js
@@ -0,0 +1,10 @@
+function requestValidator(req, res, next) {
+  if (req.headers['user-agent'] !== 'AHC/2.1') {
+    console.log('Blocked unauthorized request!');
+
+    res.status(401);
+    return res.send('Unauthorized requests!');
+  }
+
+  next();
+}
--- web.js
+++ web.js
@@ -2,11 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import router from './server/routes';
 import logger from './server/middleware/logger';
+import requestValidator from './server/middleware/requestValidator';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(logger);
+app.use(requestValidator);
 
 app.use('/', router);
 
