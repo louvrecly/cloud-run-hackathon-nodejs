ba407486ad4edfcd9c79c0b7c5212761d82f431c
Louvre Chow <louvrecly@gmail.com>
2022-06-26 19:47:14 -0400

    Simplify action function with escape and hunt services

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,4 +1,4 @@
-import { scanArena, checkEnemyInRange, scanSurroundings } from '../services';
+import { scanArena, checkEnemyInRange, scanSurroundings, escape, hunt } from '../services';
 
 function get(req, res) {
   return res.send('Let the battle begin!');
@@ -12,59 +12,14 @@ function action(req, res) {
 
   const arena = scanArena(dims, state);
   const ownState = state[href];
-  const { front, back, left, right } = scanSurroundings(ownState, arena, dims);
+  const surroundings = scanSurroundings(ownState, arena, dims);
 
-  // check if under attack
-  if (ownState.wasHit) {
-    if (
-      // left or right has enemy
-      (
-        (left.obstacle !== null && left.obstacle !== 'wall') ||
-        (right.obstacle !== null && right.obstacle !== 'wall')
-      ) &&
-      // front has no obstacle within distance 2
-      front.distance > 2
-    ) {
-      return res.send('F');
-    } else if (
-      // front or back has enemy
-      (
-        (front.obstacle !== null && front.obstacle !== 'wall') ||
-        (back.obstacle !== null && back.obstacle !== 'wall')
-      ) &&
-      // left has no obstacle within distance 2
-      left.distance > 2
-    ) {
-      return res.send('L');
-    } else {
-      return res.send('R');
-    }
-  // check if enemy within range of throw
-  } else if (checkEnemyInRange(front)) {
-    return res.send('T');
-  // look for target
-  } else {
-    if (
-      // left has enemy or right has wall
-      (left.obstacle !== null && left.obstacle !== 'wall') ||
-      right.obstacle === 'wall'
-    ) {
-      return res.send('L');
-    } else if (
-      // right has enemy or left has wall
-      (right.obstacle !== null && right.obstacle !== 'wall') ||
-      right.obstacle === 'wall'
-    ) {
-      return res.send('R');
-    } else if (
-      // front has no wall within distance 2
-      front.distance > 2
-    ) {
-      return res.send('F');
-    } else {
-      return res.send(['L', 'R'][Math.floor(Math.random()) * 2]);
-    }
-  }
+  // escape if under attack
+  if (ownState.wasHit) return res.send(escape(surroundings));
+  // throw if enemy within range of throw
+  else if (checkEnemyInRange(surroundings.front)) return res.send('T');
+  // hunt otherwise
+  else return res.send(hunt(surroundings));
 }
 
 export default { get, action };
