5ccc3cc7b3d6fc1bbbcb1d9293c063bea4c3dd8d
Louvre Chow <louvrecly@gmail.com>
2022-06-27 05:06:01 -0400

    send more gift

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -16,7 +16,7 @@ function action(req, res) {
   const forwardState = surroundings.front.distance > 1 ? getForwardState(ownState, dims) : false;
   const forwardSurroundings = forwardState && scanSurroundings(forwardState, arena, dims);
 
-  greetPeers(state, ownState, {}, 50);
+  greetPeers(state, ownState, {}, 100);
 
   return res.send(decideAction(ownState.wasHit, surroundings, forwardSurroundings));
 }
