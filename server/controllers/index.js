e67d665be6cdcb9756a18c1212ce52205aca904f
Louvre Chow <louvrecly@gmail.com>
2022-06-26 22:37:09 -0400

    Import forwardState and evaluate forwardSurroundings and pass it into hunt in action

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,4 +1,4 @@
-import { scanArena, checkEnemyInRange, scanSurroundings, escape, hunt } from '../services';
+import { scanArena, checkEnemyInRange, getForwardState, scanSurroundings, escape, hunt } from '../services';
 
 function get(req, res) {
   return res.send('Let the battle begin!');
@@ -13,13 +13,15 @@ function action(req, res) {
   const arena = scanArena(dims, state);
   const ownState = state[href];
   const surroundings = scanSurroundings(ownState, arena, dims);
+  const forwardState = surroundings.front.distance > 1 ? getForwardState(ownState, dims) : false;
+  const forwardSurroundings = forwardState && scanSurroundings(forwardState, arena, dims);
 
   // escape if under attack
   if (ownState.wasHit) return res.send(escape(surroundings));
   // throw if enemy within range of throw
   else if (checkEnemyInRange(surroundings.front)) return res.send('T');
   // hunt otherwise
-  else return res.send(hunt(surroundings));
+  else return res.send(hunt(surroundings, forwardSurroundings));
 }
 
 export default { get, action };
