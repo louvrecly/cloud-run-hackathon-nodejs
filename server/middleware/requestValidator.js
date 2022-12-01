b4416f6b19110e979ed5f6b7c57949e5f798b563
Louvre Chow <louvrecly@gmail.com>
2022-11-30 23:42:25 -0500

    Comment out requestValidator middleware

--- server/middleware/requestValidator.js
+++ server/middleware/requestValidator.js
@@ -1,16 +1,16 @@
 function requestValidator(req, res, next) {
-  const { headers } = req;
+  // const { headers } = req;
 
-  if (
-    headers['user-agent'] !== 'AHC/2.1' ||
-    headers['x-forwarded-for'] !== '34.132.127.197' ||
-    headers.forwarded !== 'for="34.132.127.197";proto=https'
-  ) {
-    console.log('Blocked unauthorized request!');
+  // if (
+  //   headers['user-agent'] !== 'AHC/2.1' ||
+  //   headers['x-forwarded-for'] !== '34.132.127.197' ||
+  //   headers.forwarded !== 'for="34.132.127.197";proto=https'
+  // ) {
+  //   console.log('Blocked unauthorized request!');
 
-    res.status(401);
-    return res.send('Unauthorized requests!');
-  }
+  //   res.status(401);
+  //   return res.send('Unauthorized requests!');
+  // }
 
   next();
 }
