d2e6174d66674368a81554610689e9dc02b9c363
Louvre Chow <louvrecly@gmail.com>
2022-12-07 17:35:00 -0500

    Apply parseState and locateLeadingEnemy to pass targetLocator to decideAction function in the controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,4 +1,4 @@
-import { scanArena, getForwardState, scanSurroundings, decideAction } from '../services';
+import { scanArena, parseState, locateLeadingEnemy, getForwardState, scanSurroundings, decideAction } from '../services';
 
 function get(req, res) {
   return res.send('Let the battle begin!');
@@ -11,12 +11,14 @@ function action(req, res) {
   } = req.body;
 
   const arena = scanArena(dims, state);
-  const ownState = state[href];
+  const parsedState = parseState(state, href);
+  const { ownState, enemyState } = parsedState;
   const surroundings = scanSurroundings(ownState, arena, dims);
+  const targetLocator = locateLeadingEnemy({ ownState, enemyState });
   const forwardState = surroundings.front.distance > 1 ? getForwardState(ownState, dims) : false;
   const forwardSurroundings = forwardState && scanSurroundings(forwardState, arena, dims);
 
-  return res.send(decideAction(ownState.wasHit, surroundings, forwardSurroundings));
+  return res.send(decideAction(ownState.wasHit, surroundings, forwardSurroundings, targetLocator));
 }
 
 export default { get, action };
